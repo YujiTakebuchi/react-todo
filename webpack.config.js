aeb26fc5eba81b86053bd6c6b0d2b2a141c397c3
YujiTakebuchi <takeyou.swift@gmail.com>
2022-10-13 16:53:35 +0900

    Modify webpack mode logic and prettierignore

--- webpack.config.js
+++ webpack.config.js
@@ -3,113 +3,108 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { glob } = require("glob");
 
+const mode = "development";
+const isDevMode = mode === "development";
+
 const entriesJS = {};
-glob
-  .sync("./src/**/entry.js", {
+glob.sync("./src/**/entry.js", {
     ignore: "./src/**/_*.js",
-  })
-  .map((file) => {
+}).map((file) => {
     console.log(file);
     const regEx = new RegExp("./src");
     const fileOriginalName = file.replace(regEx, "");
     const key = fileOriginalName.replace("entry.js", "bundle.js");
     entriesJS[key] = file;
-  });
+});
 
 const entriesSass = {};
-glob
-  .sync("./src/**/*.scss", {
+glob.sync("./src/**/*.scss", {
     ignore: "./src/**/_*.scss",
-  })
-  .map((file) => {
+}).map((file) => {
     console.log(file);
     const regEx = new RegExp("./src");
     const fileOriginalName = file.replace(regEx, "");
     const fileChangeDirName = fileOriginalName.replace("/style/", "/css/");
     const fileChangeExtName = fileChangeDirName.replace(".scss", ".css");
     entriesSass[fileChangeExtName] = file;
-  });
+});
 
 module.exports = [
-  // {
-  //   devServer: {
-  //     static: "dist",
-  //     open: true,
-  //   },
-  // },
-  {
-    entry: entriesJS,
-    output: {
-      filename: "[name]",
-      path: path.join(__dirname + "/dist"),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
+    {
+        mode: mode,
+        entry: entriesJS,
+        output: {
+            filename: "[name]",
+            path: path.join(__dirname + "/dist"),
         },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-      }),
-    ],
-    devtool: "source-map",
-    devServer: {
-      static: "dist",
-      open: true,
-    },
-  },
-
-  {
-    entry: entriesSass,
-    output: {
-      path: path.join(__dirname + "/dist/css"),
-      filename: "../../webpack/hash/[contenthash]",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-            },
-            {
-              loader: "css-loader",
-              options: {
-                url: false,
-                sourceMap: true,
-                importLoaders: 2,
-              },
-            },
-            {
-              loader: "postcss-loader",
-              options: {
-                postcssOptions: {
-                  plugins: [["autoprefixer", { grid: true }]],
+        module: {
+            rules: [
+                {
+                    test: /\.js/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
                 },
-              },
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
+            ],
         },
-      ],
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+            }),
+        ],
+        devtool: "source-map",
+        devServer: {
+            static: "dist",
+            open: true,
+        },
     },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "style.css",
-      }),
-    ],
-    target: ["web", "es5"],
-    devtool: "source-map",
-  },
+
+    {
+        mode: mode,
+        entry: entriesSass,
+        output: {
+            path: path.join(__dirname + "/dist/css"),
+            filename: "../../webpack/hash/[contenthash]",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                url: false,
+                                sourceMap: isDevMode,
+                                importLoaders: 2,
+                            },
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                postcssOptions: {
+                                    plugins: [["autoprefixer", { grid: true }]],
+                                },
+                            },
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: isDevMode,
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "style.css",
+            }),
+        ],
+        target: ["web", "es5"],
+        devtool: "source-map",
+    },
 ];
