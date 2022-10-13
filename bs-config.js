b110b3c457b46d98ba550231c06e7821da309f3f
YujiTakebuchi <takeyou.swift@gmail.com>
2022-10-13 13:19:43 +0900

    indent

--- bs-config.js
+++ bs-config.js
@@ -1,17 +1,17 @@
 const ssi = require("./node_modules/browsersync-ssi");
 
 module.exports = {
-  startPath: "./",
-  port: 3300,
-  files: ["./dist/**/*.css", "./dist/**/*.html", "./dist/**/*.js"],
-  server: {
-    baseDir: "./dist/",
-  },
-  middleware: ssi({
-    baseDir: "./dist/",
-    ext: ".html",
-  }),
-  ghostMode: false,
-  browser: "google chrome",
-  open: "local",
+    startPath: "./",
+    port: 3300,
+    files: ["./dist/**/*.css", "./dist/**/*.html", "./dist/**/*.js"],
+    server: {
+        baseDir: "./dist/",
+    },
+    middleware: ssi({
+        baseDir: "./dist/",
+        ext: ".html",
+    }),
+    ghostMode: false,
+    browser: "google chrome",
+    open: "local",
 };
--- src/js/components/StarRating.js
+++ src/js/components/StarRating.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
-import { FaStar } from "react-icons/fa"
+import { FaStar } from "react-icons/fa";
 
-const Star = ({ selected = false, onSelect = f => f }) => (
+const Star = ({ selected = false, onSelect = (f) => f }) => (
     <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
-)
+);
 
-export default function StarRating({ style={}, totalStars = 5, ...props }) {
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
     const [selectedStars, setSelectedStars] = useState(0);
     return (
         <div style={{ padding: "5px", ...style }} {...props}>
-            {
-                [...Array(totalStars).fill()].map((n, i) => (
-                    <Star
-                        key={i}
-                        selected={selectedStars > i}
-                        onSelect={() => setSelectedStars(i + 1)}
-                    />
-                ))
-            }
+            {[...Array(totalStars).fill()].map((n, i) => (
+                <Star
+                    key={i}
+                    selected={selectedStars > i}
+                    onSelect={() => setSelectedStars(i + 1)}
+                />
+            ))}
             <p>
                 {selectedStars} of {totalStars} stars
             </p>
